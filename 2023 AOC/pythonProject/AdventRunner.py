7d935f3bcc325fdf08727bed4c86951953b5b672
mkeilman <mvkeilman+github@gmail.com>
2024-07-11 10:17:08 -0600

    runner

--- 2023 AOC/pythonProject/AdventRunner.py
+++ 2023 AOC/pythonProject/AdventRunner.py
@@ -3,12 +3,12 @@ import Day
 
 
 def main():
-    day = 4
+    day = 5
     d = importlib.import_module(f"Day_{day:02d}").AdventDay()
     print("TEST:")
     d.run_from_test_strings()
-    print("FILE:")
-    d.run_from_file()
+    #print("FILE:")
+    #d.run_from_file()
 
 
 if __name__ == "__main__":
