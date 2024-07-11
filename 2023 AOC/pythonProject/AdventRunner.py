a1c76dc933e1b463d7ed74612aa0e2b699f76f32
mkeilman <mvkeilman+github@gmail.com>
2024-07-11 10:53:02 -0600

    arg parsing

--- 2023 AOC/pythonProject/AdventRunner.py
+++ 2023 AOC/pythonProject/AdventRunner.py
@@ -1,14 +1,27 @@
+import argparse
 import importlib
 import Day
 
 
 def main():
-    day = 5
-    d = importlib.import_module(f"Day_{day:02d}").AdventDay()
-    print("TEST:")
-    d.run_from_test_strings()
-    #print("FILE:")
-    #d.run_from_file()
+    p = argparse.ArgumentParser(description='run AdventOfCode for the given day')
+    p.add_argument("day", type=int, help="day to run")
+    p.add_argument(
+        "--mode",
+        type=str,
+        help="run mode",
+        choices=["test", "file", "all"],
+        default="all",
+        dest="mode",
+    )
+    a = p.parse_args()
+    d = importlib.import_module(f"Day_{a.day:02d}").AdventDay()
+    if a.mode in ("test", "all"):
+        print("TEST:")
+        d.run_from_test_strings()
+    if a.mode in ("file", "all"):
+        print("FILE:")
+        d.run_from_file()
 
 
 if __name__ == "__main__":
