ab91435164c1349a8980d0b9ac26d2a0367b51fd
mkeilman <mvkeilman+github@gmail.com>
2024-10-24 14:57:21 -0600

    day 7 jokers

--- year_2023/Day_07.py
+++ year_2023/Day_07.py
@@ -5,10 +5,12 @@ import Day
 
 class CamelCard():
     labels = "23456789TJQKA"
+    joker_labels = "J23456789TQKA"
 
-    def __init__(self, label):
+    def __init__(self, label, jokers=True):
         self.label = label
-        self.rank = {x:CamelCard.labels.index(x) for x in CamelCard.labels}[label]
+        ll = CamelCard.joker_labels if jokers else CamelCard.labels
+        self.rank = {x:ll.index(x) for x in ll}[label]
 
     def cmp(self, other_card):
         return 1 if self.rank > other_card.rank else -1 if self.rank < other_card.rank else 0
@@ -19,43 +21,29 @@ class CamelHand():
     @classmethod
     def cmp(cls, h1, h2):
         return h1.cmp(h2)
-        if h1.hand_type != h2.hand_type:
-            return 1 if h1.hand_type > h2.hand_type else -1
-        res = h1.cards[0].cmp(h2[0])
-        if len(h2) == 1 or res != 0:
-            return res
-        #return h1.sub_hand().cmp(h2.sub_hand())
-        return cls.cmp(h1.sub_hand(), h2.sub_hand())
 
-    def __init__(self, hand_str):
+    def __init__(self, hand_str, jokers=True):
         self.hand_str = hand_str
-        self.cards = [CamelCard(x) for x in hand_str]
+        self.jokers = jokers
+        self.cards = [CamelCard(x, jokers=jokers) for x in hand_str]
         self.cards_sorted = sorted(self.cards, key=lambda x: x.rank)
         self.card_groups = self._group_cards_by_rank()
         self.hand_type = self._type()
         self.ranks = [x.rank for x in self.cards]
-        #print(self.hand_str, self.hand_type)
-        #print("GRPS")
-        #for g in self.card_groups:
-        #    print([x.label for x in g])
-
-    def sub_hand(self):
-        return CamelHand(self.hand_str[1:])
     
     def cmp(self, other_hand):
-        def _card_cmp(h1, h2):
-            res = h1.cards[0].cmp(h2.cards[0])
-            if len(h2.cards) == 1 or res != 0:
+        def _card_cmp(c1, c2):
+            #print("CRD CMP", "".join([x.label for x in c1]), "".join([x.label for x in c2]))
+            res = c1[0].cmp(c2[0])
+            if len(c2) == 1 or res != 0:
                 return res
-            return _card_cmp(h1.sub_hand(), h2.sub_hand())
+            return _card_cmp(c1[1:], c2[1:])
         
         if self.hand_type != other_hand.hand_type:
             return 1 if self.hand_type > other_hand.hand_type else -1
-        return _card_cmp(self, other_hand)
-        #res = self.cards[0].cmp(other_hand.cards[0])
-        #if len(other_hand.cards) == 1 or res != 0:
-        #    return res
-        #return self.sub_hand().cmp(other_hand.sub_hand())
+        #print("CRD CMP", self.hand_str, other_hand.hand_str)
+        return _card_cmp(self.cards, other_hand.cards)
+
     
     def _group_cards_by_rank(self):
         card_groups = []
@@ -75,7 +63,8 @@ class CamelHand():
             last_card = card
 		
         card_groups.append(card_group)
-        return sorted(card_groups, key=len, reverse=True)
+        #return sorted(card_groups, key=len, reverse=True)
+        return sorted(card_groups, key=lambda x: (len(x), x[0].rank), reverse=True)
 	
     def _is_type_3_of_a_kind(self):
         return len(self.card_groups) >= 2 and len(self.card_groups[0]) == 3 and len(self.card_groups[1]) == 1
@@ -99,17 +88,36 @@ class CamelHand():
         return len(self.card_groups) >= 2 and len(self.card_groups[0]) == 2 and len(self.card_groups[1]) == 2
 
     def _type(self):
+
+        n = len([x for x in self.hand_str if x == "J"])
+        
+        if self.jokers and n and len(self.card_groups) > 1:
+            h = f"{self.hand_str}"
+            n_repl = 0
+            for g in self.card_groups:
+                if n <= 0:
+                    break
+                label = g[0].label
+                if label == "J":
+                    continue
+                nr = min(n, 5 - len(g))
+                h = h.replace("J", label, nr)
+                n -= nr
+                n_repl += 1
+            #print(self.hand_str, h)
+            return CamelHand(h, jokers=False).hand_type
+
         for i, f in enumerate([
-            self._is_type_high_card,
-            self._is_type_pair,
-            self._is_type_two_pair,
-            self._is_type_3_of_a_kind,
-            self._is_type_full_house,
-            self._is_type_4_of_a_kind,
             self._is_type_5_of_a_kind,
+            self._is_type_4_of_a_kind,
+            self._is_type_full_house,
+            self._is_type_3_of_a_kind,
+            self._is_type_two_pair,
+            self._is_type_pair,
+            self._is_type_high_card,
         ]):
             if f():
-                return i
+                return 6 - i
         return -1
 
 class Play():
@@ -117,10 +125,10 @@ class Play():
     def cmp(cls, p1, p2):
         return CamelHand.cmp(p1.hand, p2.hand)
     
-    def __init__(self, play_str):
+    def __init__(self, play_str, jokers=True):
         re_hand_bet = fr"([{CamelCard.labels}]+)\s?(\d+)"
         hb = re.match(re_hand_bet, play_str)
-        self.hand = CamelHand(hb.group(1))
+        self.hand = CamelHand(hb.group(1), jokers=jokers)
         self.bet = int(hb.group(2))
 
 
@@ -128,8 +136,6 @@ class AdventDay(Day.Base):
      
     @classmethod
     def _parse_line(cls, line, preserve_spaces):
-        
-
         return [int(reduce((lambda x, y: x + y), line, ""))]
 
     def __init__(self, run_args):
@@ -138,6 +144,8 @@ class AdventDay(Day.Base):
             2023,
             7,
             [
+                #"22922 405",
+                #"22J92 630",
                 "32T3K 765",
                 "T55J5 684",
                 "KK677 28",
@@ -145,12 +153,24 @@ class AdventDay(Day.Base):
                 "QQQJA 483",
             ]
         )
-    
+        self.args_parser.add_argument(
+            "--jokers",
+            action=argparse.BooleanOptionalAction,
+            default=True,
+            dest="jokers",
+        )
+        self.jokers = self.args_parser.parse_args(run_args).jokers
 
     def run(self, v):
         import functools
-        plays = sorted([Play(x) for x in v], key=functools.cmp_to_key(Play.cmp))
+        plays = sorted([Play(x, jokers=self.jokers) for x in v], key=functools.cmp_to_key(Play.cmp))
         #print(f"PLAYS {[p.hand.hand_str for p in plays]}")
+        #print(f"HIGH {[p.hand.hand_str for p in plays if p.hand.hand_type == 0]}")
+        #print(f"PAIR {[p.hand.hand_str for p in plays if p.hand.hand_type == 1]}")
+        #print(f"TWO PAIR {[p.hand.hand_str for p in plays if p.hand.hand_type == 2]}")
+        #print(f"THREE OF A KIND {[p.hand.hand_str for p in plays if p.hand.hand_type == 3]}")
+        #print(f"FULL HOUSE {[p.hand.hand_str for p in plays if p.hand.hand_type == 4]}")
+        print(f"FOUR OF A KIND {[p.hand.hand_str for p in plays if p.hand.hand_type == 5]}")
         amts = [(i + 1) * x for (i, x) in enumerate([y.bet for y in plays])]
         winnings = reduce((lambda x, y: x + y), amts, 0)
         print(f"WIN {winnings}")
