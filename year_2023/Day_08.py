614e75ba6e5a69afc36d62737511bc13f6b7e2ae
mkeilman <mvkeilman+github@gmail.com>
2024-11-09 08:09:18 -0700

    day 8 ghost routes

--- year_2023/Day_08.py
+++ year_2023/Day_08.py
@@ -4,7 +4,7 @@ import Day
 
 
 class RouteNode():
-    re_3 = r"[A-Z][A-Z][A-Z]"
+    re_3 = r"[A-Z0-9]{3}"
     re_node = fr"({re_3})\s*=\s*\(({re_3}),\s*({re_3})\)"
 
     def __init__(self, node_str):
@@ -21,21 +21,49 @@ class RouteMap():
         self.directions = directions.strip()
         self.route_nodes = route_nodes
         self.route_dict = {n.name: n for n in self.route_nodes}
-        #assert len(set(self.node_names)) == len(self.node_names)
 
-
+    def _next_node(self, node, direction_index):
+        return self.route_dict[node.routes[self.directions[direction_index]]]
+    
+    def _next_dir(self, direction_index):
+        return (direction_index + 1) % len(self.directions)
+    
     def get_route(self, start_name, end_name):
-        r = []
         i = 0
         n = 0
         curr = self.route_dict[start_name]
         next = curr
         while next.name != end_name:
-            next = self.route_dict[curr.routes[self.directions[i]]]
-            i = (i + 1) % len(self.directions)
+            next = self._next_node(curr, i)
+            i = self._next_dir(i)
             n += 1
             curr = next
         return n
+    
+    def get_ghost_route(self):
+        import math
+
+        start_nodes = [x for x in self.route_nodes if x.name[-1] == "A"]
+        end_nodes = [x for x in self.route_nodes if x.name[-1] == "Z"]
+        print([x.name for x in start_nodes])
+        print([x.name for x in end_nodes])
+
+        i = 0
+        n = 1
+
+        c = []
+        for node in start_nodes:
+            i = 0
+            m = 0
+            nn = node
+            while nn not in end_nodes:
+                nn = self._next_node(nn, i)
+                i = self._next_dir(i)
+                m += 1
+            n *= m
+            c.append(m)
+
+        return math.lcm(*c)
 
 
 class AdventDay(Day.Base):
@@ -46,16 +74,28 @@ class AdventDay(Day.Base):
             2023,
             8,
             [
-                "RL",
+                "LR",
                 "",
-                "AAA = (BBB, CCC)",
-                "BBB = (DDD, EEE)",
-                "CCC = (ZZZ, GGG)",
-                "DDD = (DDD, DDD)",
-                "EEE = (EEE, EEE)",
-                "GGG = (GGG, GGG)",
-                "ZZZ = (ZZZ, ZZZ)",
+                "11A = (11B, XXX)",
+                "11B = (XXX, 11Z)",
+                "11Z = (11B, XXX)",
+                "22A = (22B, XXX)",
+                "22B = (22C, 22C)",
+                "22C = (22Z, 22Z)",
+                "22Z = (22B, 22B)",
+                "XXX = (XXX, XXX)",
             ]
+            #[
+            #    "RL",
+            #    "",
+            #    "AAA = (BBB, CCC)",
+            #    "BBB = (DDD, EEE)",
+            #    "CCC = (ZZZ, GGG)",
+            #    "DDD = (DDD, DDD)",
+            #    "EEE = (EEE, EEE)",
+            #    "GGG = (GGG, GGG)",
+            #    "ZZZ = (ZZZ, ZZZ)",
+            #]
         )
         self.args_parser.add_argument(
             "--start-node",
@@ -73,7 +113,8 @@ class AdventDay(Day.Base):
 
     def run(self, v):
         self.route_map = RouteMap(v[0], [RouteNode(x) for x in v[2:]])
-        print(f"N STEPS {self.route_map.get_route(self.start_node, self.end_node)}")
+        print(f"GHOST STEPS {self.route_map.get_ghost_route()}")
+        #print(f"STEPS {self.route_map.get_route(self.start_node, self.end_node)}")
 
 
 def main():
