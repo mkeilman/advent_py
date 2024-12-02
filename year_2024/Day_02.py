8bc78a075c86698fbd1219b188dfcf06d61956ad
mkeilman <mvkeilman+github@gmail.com>
2024-12-02 09:02:53 -0700

    2024 day 2 dampen

--- year_2024/Day_02.py
+++ year_2024/Day_02.py
@@ -5,22 +5,32 @@ import Utils
 
 class AdventDay(Day.Base):
 
-    def safe_sum(self, v):
+    def safe_sum(self, v, dampen=True):
         d = [[int(y) for y in re.findall(r"\d+", x)] for x in v]
-        s = [int(self._is_safe(x)) for x in d]
+        s = [int(self._is_safe(x, dampen=dampen)) for x in d]
         return Utils.Math.sum(s)
 
     
-    def _is_safe(self, arr):
+    def _is_safe(self, arr, dampen=True):
         max_diff = 3
         min_diff = 1
 
-        d = [arr[i + 1] - arr[i] for i, _ in enumerate(arr[1:])]
-        if not all([Utils.Math.sign(x) == Utils.Math.sign(d[0]) for x in d]):
+        def _s(arr):
+            d = [arr[i + 1] - arr[i] for i, _ in enumerate(arr[1:])]
+            return all([Utils.Math.sign(x) == Utils.Math.sign(d[0]) for x in d]) and all([min_diff <= abs(x) <= max_diff for x in d])
+
+        s = _s(arr)
+        if not s and dampen:
+            t = False
+            for i in range(len(arr)):
+                t = _s(arr[:i] + arr[i + 1:])
+                if t:
+                    return True
             return False
-        return all([min_diff <= abs(x) <= max_diff for x in d])
+        return s
 
     def __init__(self, run_args):
+        import argparse
         super(AdventDay, self).__init__(
             2024,
             2,
@@ -33,9 +43,16 @@ class AdventDay(Day.Base):
                 "1 3 6 7 9",
             ]
         )
+        self.args_parser.add_argument(
+            "--dampen",
+            action=argparse.BooleanOptionalAction,
+            default=True,
+            dest="dampen",
+        )
+        self.dampen = self.args_parser.parse_args(run_args).dampen
 
     def run(self, v):
-        print(f"SAFE {self.safe_sum(v)}")
+        print(f"SAFE {self.safe_sum(v, dampen=self.dampen)} D? {self.dampen}")
 
 
 def main():
