46658c3874a79c342fdf805f9cdbe3cc67d603f9
mkeilman <mvkeilman+github@gmail.com>
2024-12-02 08:34:58 -0700

    added sign()

--- Utils.py
+++ Utils.py
@@ -6,8 +6,14 @@ class Math:
         pass
 
     @staticmethod
+    def sign(val):
+        return -1 if val < 0 else (1 if val > 0 else 0)
+    
+    @staticmethod
     def sum(arr, init_val=0):
         return functools.reduce((lambda x, y: x + y), arr, init_val)
 
+    @staticmethod
     def product(arr, init_val=1):
         return functools.reduce((lambda x, y: x * y), arr, init_val)
+
