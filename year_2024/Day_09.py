85bdad1789efebef3d769af75f16fcf843e2d10e
mkeilman <mvkeilman+github@gmail.com>
2024-12-13 14:09:16 -0700

    2024 day 9 ckp

--- year_2024/Day_09.py
+++ year_2024/Day_09.py
@@ -8,16 +8,34 @@ class Disk:
 
     def __init__(self, txt):
         self.map = txt
-        s = re.findall(r"\d", txt)
-        self.files = [int(x) for i, x in enumerate(s) if not i % 2]
-        self.spaces = [int(x) for i, x in enumerate(s) if i % 2]
+        s = [int(x) for x in re.findall(r"\d", self.map)]
+        self.map_arr = s
+        f = [x for i, x in enumerate(s) if not i % 2]
+        num_files = len(f)
+        debug(f"N FILES {num_files}")
+        sp = [x for i, x in enumerate(s) if i % 2]
+        num_spaces = len(sp)
+        self.space_inds = [math.sum(f[:i]) + math.sum(sp[:i - 1]) for i in range(1, num_files)]
+        self.size = math.sum(f)
+        debug(f"SZ {self.size}")
         self.blocks = self._blocks()
         self.defragged = self._defrag()
+        debug(f"DF SZ {len(self.defragged)}")
         self.checksum = self._checksum()
+        self.df = [None] * (num_files + num_spaces)
         
 
     def _blocks(self):
         s = ""
+        a = []
+        for i, c in enumerate(self.map_arr):
+            if not i % 2:
+                a.extend(c * [i // 2])
+            else:
+                a.extend(c * [-1])
+        return a
+
+
         for i, c in enumerate(self.map):
             if not i % 2:
                 s += int(c) * str(i // 2)
@@ -26,27 +44,31 @@ class Disk:
         return s
     
     def _checksum(self):
-        n = 0
-        for i, c in enumerate(self.defragged):
-            n += i * int(c)
-        return n
+        #return math.sum([i * int(x) for i, x in enumerate(self.defragged)])
+        return math.sum([i * x for i, x in enumerate(self.defragged)])
 
     def _defrag(self):
         n = len(self.blocks)
         n_moves = 0
-        arr = re.findall(r".", self.blocks)
+        #arr = re.findall(r".", self.blocks)
+        arr = self.blocks[:]
         for i in range(n):
             j = n - i - 1
             c = self.blocks[j]
-            if c == ".":
+            if c == -1:
+            #if c == ".":
                 continue
-            k = arr.index(".")
+            #k = arr.index(".")
+            k = arr.index(-1)
             if k > j:
                 break
             arr[k] = c
-            arr[j] = "."
+            #arr[j] = "."
+            arr[j] = -1
+            #debug("".join(arr))
             n_moves += 1
-        return "".join(arr).replace(".", "")
+        #return "".join(arr).replace(".", "")
+        return [x for x in arr if x >= 0]
             
 
 
@@ -75,7 +97,7 @@ class AdventDay(Day.Base):
     def run(self, v):
         # single line
         d = Disk(v[0])
-        debug(f"C {d.defragged}")
+        debug(f"C {d.checksum}")
 
 
 
