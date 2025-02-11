f48a3e62a6a691dee138d3c75ad9cbe051256f8e
mkeilman <mvkeilman+github@gmail.com>
2025-02-11 09:17:41 -0700

    2024 Day 18

--- year_2024/Day_18.py
+++ year_2024/Day_18.py
@@ -38,17 +38,43 @@ class MemorySpace:
 
     def paths(self):
 
-        def _amend_paths(base_path, unused_connections, depth=0, all_paths=[]):
-            for pos in [x for x in unused_connections if x in base_path]:
+        def _amend_paths(base_path, unused_connections, depth=0, all_paths=[], rejected_paths=[]):
+            debug(f"{depth} ALL {len(all_paths)} REJ {len(rejected_paths)} NUM U {len(unused_connections)}")
+            n = 0
+            ux = [x for x in unused_connections if x in base_path]
+            nux = len(ux)
+            for pos in ux:
+                n += 1
                 i = base_path.index(pos)
+                #if i + 1 >= _max_path_len(all_paths):
+                #    debug(f"{depth} INIT PATH TOO LONG {i + 1} >= {_max_path_len(all_paths)}")
+                #    break
                 initial_path = base_path[:i + 1]
+                #debug(f"{depth} POS N {n}")
                 new_path, u = _path(initial_path, self.end_pos, exclusions={pos: [base_path[i + 1]]}, max_len=_max_path_len(all_paths))
                 if not new_path or not u:
                     continue
-                if new_path not in all_paths:
+                if new_path in rejected_paths:
+                    debug(f"{depth} {n}/{nux} ALREADY REJECTED")
+                    continue
+                if new_path in all_paths:
+                    debug(f"{depth} {n}/{nux} ALREADY ADDED")
+                    continue
+                #if new_path not in all_paths:
+                if len(new_path) < _max_path_len(all_paths):
+                    #debug(f"{depth} OK TO ADD NEW {len(new_path)} < {_max_path_len(all_paths)}")
+                    self.display_path(new_path)
                     all_paths.append(new_path)
-                    debug(f"{depth} ADD NEW {len(new_path)} MAX {_max_path_len(all_paths)}")
-                    _amend_paths(new_path, u, depth=depth + 1, all_paths=all_paths)
+                #debug(f"{depth} ADD NEW {len(new_path)} MAX {_max_path_len(all_paths)}")
+                else:
+                    debug(f"{depth} {n}/{nux} TOO LONG {len(new_path)} >= {_max_path_len(all_paths)}")
+                    rejected_paths.append(new_path)
+                uu = [x for x in u if x in new_path and new_path.index(x) + 1 < _max_path_len(all_paths)]
+                if not uu:
+                    debug(f"{depth} {n}/{nux} NO UNUSED")
+                    continue
+                _amend_paths(new_path, uu, depth=depth + 1, all_paths=all_paths, rejected_paths=rejected_paths)
+            debug(f"{depth} {n}/{nux} DONE AMEND")
             return
 
         def _exclude(pos, connection, exclusions):
@@ -65,10 +91,16 @@ class MemorySpace:
         def _path(initial_path, end_pos, exclusions={}, max_len=1e26):
             pos0 = initial_path[-1]
             pos = pos0
-            pp = initial_path
+            pp = initial_path[:]
             done = False
             unused = {}
             while not done and pos != end_pos:
+                if len(pp) >= max_len:
+                    #debug(f"TOO LONG FROM {pos} - {pp[-1]}: {len(pp)}")
+                    pos = _trim_to_prev_branch(pp, exclusions, limit=pos)
+                    #debug(f"TRIMMED TO {pos} L {len(pp)}")
+                    if pos is None:
+                        break
                 # loop through the connections to this position, not counting
                 # the preivous position and excluded positions
                 p_con = _conn(pos, pp, exclusions=exclusions)
@@ -80,11 +112,15 @@ class MemorySpace:
                     pos = p
                     break
                 else:
+                    #debug(f"NO BRANCH: {pos}")
                     pos = _trim_to_prev_branch(pp, exclusions, limit=pos0)
                     done = not pos
 
             if pos != end_pos:
                 return [], None
+            if pp == initial_path:
+                debug("SAME??")
+                #return [], None
             return pp, unused
 
         def _prev_branch(path, exclusions={}, limit=None):
@@ -125,7 +161,7 @@ class MemorySpace:
 
         paths = []
         path, unused = _path([start], self.end_pos)
-        debug(f"FIRST {path} L {len(path)}")
+        debug(f"FIRST L {len(path)}")
         #self.display_path(path)
         paths.append(path)
         _amend_paths(path, unused, all_paths=paths)
@@ -190,8 +226,8 @@ class AdventDay(Day.Base):
         m = MemorySpace(v, self.args["size"], self.args["num_bytes"])
         paths = m.paths()
         pr = sorted(paths, key=lambda x: len(x))
-        debug(f"FOUND {len(paths)} PATHS")
-        debug(f"SHORTEST PATH LEN {len(pr[0]) - 1}")
+        debug(f"FOUND {len(paths)} PATHS LENS {[len(x) for x in pr]}")
+        debug(f"SHORTEST NUM STEPS {len(pr[0]) - 1}")
         m.display_path(pr[0])
         
 
