e93eaad05ad265d3b6cbca80a2573b2eb022f182
mkeilman <mvkeilman+github@gmail.com>
2025-04-01 09:35:19 -0600

    day 11 tests

--- year_2024/tests/DayTests.py
+++ year_2024/tests/DayTests.py
@@ -6,13 +6,30 @@ def _day():
     import re
     import sys
     import Day
-    from Day import Base
     yd = [int(x) for x in re.match(r"test_day_(\d+)_(\d+)", sys._getframe(1).f_code.co_name).groups()]
     return Day.Base.get_day(*yd, {})
 
 
 def test_day_2024_11():
     d = _day()
-    d.run_from_test_strings()
-    pass
+
+    d.set_num_blinks(1)
+    n = d.run_from_test_strings(substitute_strings=["0 1 10 99 999"])
+    assert n == 7
+
+    d.set_num_blinks(6)
+    n = d.run_from_test_strings(substitute_strings=["125 17"])
+    assert n == 22
+
+    d.set_num_blinks(25)
+    n = d.run_from_test_strings(substitute_strings=["125 17"])
+    assert n == 55312
+
+    n = d.run_from_file()
+    assert n == 203457
+
+    d.set_num_blinks(75)
+    n = d.run_from_file()
+    assert n == 241394363462435
+
 
