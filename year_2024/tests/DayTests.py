d77d268d62201828b155b3e1a745a9fe6ed5ed0d
mkeilman <mvkeilman+github@gmail.com>
2025-04-01 10:09:37 -0600

    moved to test class, added fixture

--- year_2024/tests/DayTests.py
+++ year_2024/tests/DayTests.py
@@ -9,13 +9,6 @@ def day(request):
     return Day.Base.get_day(*yd, {})
 
 
-def _day():
-    import re
-    import sys
-    import Day
-    yd = [int(x) for x in re.match(r"test_day_(\d+)_(\d+)", sys._getframe(1).f_code.co_name).groups()]
-    return Day.Base.get_day(*yd, {})
-
 
 class Test_Day_2024_11:
     
@@ -48,25 +41,4 @@ class Test_Day_2024_11:
         assert n == 241394363462435
 
 
-def test_day_2024_11():
-    d = _day()
-
-    d.set_num_blinks(1)
-    n = d.run_from_test_strings(substitute_strings=["0 1 10 99 999"])
-    assert n == 7
-
-    d.set_num_blinks(6)
-    n = d.run_from_test_strings(substitute_strings=["125 17"])
-    assert n == 22
-
-    d.set_num_blinks(25)
-    n = d.run_from_test_strings(substitute_strings=["125 17"])
-    assert n == 55312
-
-    n = d.run_from_file()
-    assert n == 203457
-
-    d.set_num_blinks(75)
-    n = d.run_from_file()
-    assert n == 241394363462435
 
