44beacc5cf22c9828ec7144c133a7e566dc9552e
mkeilman <mvkeilman+github@gmail.com>
2025-04-14 15:37:13 -0600

    day 19 ckp

--- year_2024/Day_19.py
+++ year_2024/Day_19.py
@@ -37,7 +37,7 @@ class AdventDay(Day.Base):
         self.add_args(run_args)
 
     def run(self):
-        self.input = AdventDay.OVERLAP
+        #self.input = AdventDay.OVERLAP
         self._parse()
         n = self._num_good_towels()
         debug_print(f"GOOD TOWELS {n} / {len(self.towels)}")
@@ -46,52 +46,72 @@ class AdventDay(Day.Base):
     def _check_towel(self, towel):
         i = 0
         s = ""
-        t = ""
+        #t = ""
         ta = []
         found = False
+        k = 0
+        tries = 0
         while i < len(towel):
             found = False
-            for p in self.patterns:
+            #for p in self.patterns:
+            while k < len(self.patterns):
+                p = self.patterns[k]
                 j = i + len(p)
                 tt = towel[i:j]
                 #debug_print(f"CHECK {tt} VS {p}")
+                k += 1
                 if tt == p:
                     #debug_print(f"FOUND {p} IN {towel}")
-                    t += p
+                    #t += p
                     ta.append(p)
                     i = j
                     found = True
+                    k = 0
                     break
-            if not found:
-                debug_print(f"BAD {towel} {ta}")
-                break
-            #s += towel[i]
-            #if len(s) > self.max_pattern_len:
-            #    debug_print(f"{s} TOO LONG FOR {towel}")
-            #    return False
-            #if s in self.patterns:
-            #    t += s
-            #    #debug_print(f"FOUND {s} IN {towel}")
-            #    s = ""
-            #i += 1
-        #debug_print([self.patterns.index(x) for x in t])
-        #debug_print(max([len(x) for x in t]))
-        
-        #if found:
-        #    #debug_print(f"GOOD {towel}")
+            else:
+                # try next pattern
+                tries += 1
+                if not tries % 100000:
+                    debug_print(f"{tries} BAD {towel} {ta} I {i} J {j} K {k} P {p}")
+                #debug_print(f"BAD {towel} {ta} I {i} J {j} K {k} P {p}")
+                if not len(ta):
+                    #debug_print(f"NO MATCHES {towel}")
+                    break
+                while ta and len(p) <= 1:
+                    p = ta.pop()
+                    i -= len(p)
+                    #debug_print(f"TRY SHORTER THAN {p} {ta} I {i}")
+                kp = [x for x in self.patterns if len(x) < len(p)]
+                if not kp:
+                    #debug_print(f"ALL SINGLES {towel}")
+                    break
+                k = self.patterns.index(kp[0])
+                #else:
+                #    break
+                #break
+        if found:
+            debug_print(f"GOOD {towel}")
+        else:
+            debug_print(f"BAD {towel}")
         #    debug_print(f"IN {towel} OUT {''.join(t)} EQ? {towel == t}")
         return found
     
     
     def _num_good_towels(self):
-        return len([y for y in [self._check_towel(x) for x in self.towels] if y])
-
+        return len([y for y in [self._check_towel(x) for x in self.towels[8:9]] if y])
 
     def _parse(self):
         self.patterns = sorted(re.split(r",\s*", self.input[0]), key=len, reverse=True)
-        self.max_pattern_len = max([len(x) for x in self.patterns])
+        self.pattern_lens = set([len(x) for x in self.patterns])
+        self.max_pattern_len = max(self.pattern_lens)
+        self.patterns_by_len = self._patterns_by_len()
+        #debug_print(self.patterns_by_len)
         self.towels = self.input[2:]
         #debug_print(f"P {self.patterns} MAX {self.max_pattern_len}")
     
 
-
+    def _patterns_by_len(self):
+        p = []
+        for l in self.pattern_lens:
+            p.append([x for x in self.patterns if len(x) == l])
+        return sorted(p, key=lambda x: len(x[0]), reverse=True)
