78053d939336e72f05a89c17b987b35e7550e3ae
mkeilman <mvkeilman+github@gmail.com>
2025-04-15 08:43:02 -0600

    day 19 fixup

--- year_2024/Day_19.py
+++ year_2024/Day_19.py
@@ -44,51 +44,53 @@ class AdventDay(Day.Base):
         return n
     
     def _check_towel(self, towel):
-        i = 0
-        s = ""
-        #t = ""
+        #i = 0
+        i = len(towel) - 1
         ta = []
         found = False
         k = 0
         tries = 0
-        while i < len(towel):
+        #while i < len(towel):
+        while i >= 0:
             found = False
-            #for p in self.patterns:
             while k < len(self.patterns):
                 p = self.patterns[k]
-                j = i + len(p)
-                tt = towel[i:j]
+                #j = i + len(p)
+                j = i - len(p) + 1
+                #tt = towel[i:j]
+                tt = towel[j:i + 1]
                 #debug_print(f"CHECK {tt} VS {p}")
                 k += 1
                 if tt == p:
                     #debug_print(f"FOUND {p} IN {towel}")
-                    #t += p
-                    ta.append(p)
-                    i = j
+                    #ta.append(p)
+                    ta.insert(0, p)
+                    i = j - 1
                     found = True
                     k = 0
                     break
             else:
                 # try next pattern
                 tries += 1
-                if not tries % 100000:
-                    debug_print(f"{tries} BAD {towel} {ta} I {i} J {j} K {k} P {p}")
-                #debug_print(f"BAD {towel} {ta} I {i} J {j} K {k} P {p}")
+                #if not tries % 100:
+                #    break
+                #    #debug_print(f"{tries} BAD {towel} {ta} I {i}/{len(towel)} J {j}")
+                #tries += 1
+                #debug_print(f"{tries} BAD {towel} {ta} I {i}/{len(towel)} J {j}")
                 if not len(ta):
-                    #debug_print(f"NO MATCHES {towel}")
                     break
+                # find last pattern with at least 2 characters - if we found a match on a
+                # single character we know we cannot replace it with another
                 while ta and len(p) <= 1:
-                    p = ta.pop()
-                    i -= len(p)
-                    #debug_print(f"TRY SHORTER THAN {p} {ta} I {i}")
+                    p = ta.pop(0)
+                    #i -= len(p)
+                    i += len(p)
+                    debug_print(f"{towel} TRY SHORTER THAN {p} {ta} I {i}")
                 kp = [x for x in self.patterns if len(x) < len(p)]
                 if not kp:
                     #debug_print(f"ALL SINGLES {towel}")
                     break
                 k = self.patterns.index(kp[0])
-                #else:
-                #    break
-                #break
         if found:
             debug_print(f"GOOD {towel}")
         else:
@@ -98,9 +100,11 @@ class AdventDay(Day.Base):
     
     
     def _num_good_towels(self):
-        return len([y for y in [self._check_towel(x) for x in self.towels[8:9]] if y])
+        return len([y for y in [self._check_towel(x) for x in self.towels] if y])
+
 
     def _parse(self):
+        #self.patterns = sorted(re.split(r",\s*", self.input[0]), key=lambda x: (len(x), x), reverse=True)
         self.patterns = sorted(re.split(r",\s*", self.input[0]), key=len, reverse=True)
         self.pattern_lens = set([len(x) for x in self.patterns])
         self.max_pattern_len = max(self.pattern_lens)
