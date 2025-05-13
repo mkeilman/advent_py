fe21261dff82e23cc8b3f7ff8847e35ef7817838
mkeilman <mvkeilman+github@gmail.com>
2025-05-13 09:01:25 -0600

    added get_chars

--- utils/string.py
+++ utils/string.py
@@ -1,4 +1,30 @@
 from utils.debug import debug_print
+import utils.mathutils
+
+# get the given number of characters from an array of strings,
+# starting at the index and crossing elements if need be
+def get_chars(arr, index=0, offset=0, num_chars=1):
+    if not arr or num_chars <= 0:
+        return None, None, None
+
+    arr_str = "".join(arr)
+    start = utils.mathutils.sum([len(x) for x in arr[:index]]) + offset
+    end = start + num_chars
+    if end > len(arr_str):
+        return None, None, None
+
+    r = num_chars - len(arr[index]) + offset
+    while index < len(arr) and r >= 0:
+        index += 1
+        r -= len(arr[index]) if index < len(arr) else 0
+    if index >= len(arr):
+        index = None
+        offset = None
+    else:
+        offset = len(arr[index]) + r
+
+    return arr_str[start:end], index, offset
+
 
 def indices(sub_txt, txt):
     done = False
