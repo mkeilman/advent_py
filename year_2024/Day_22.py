a2062c75b3326a37d8ae377a5df9b6d1591facb2
mkeilman <mvkeilman+github@gmail.com>
2025-08-01 09:07:53 -0600

    2024 day 22 pt 2 speedup

--- year_2024/Day_22.py
+++ year_2024/Day_22.py
@@ -26,6 +26,7 @@ class AdventDay(Day.Base):
 
     def __init__(self, run_args):
         super(AdventDay, self).__init__(2024, 22)
+        self.change_group_size = 4
         self.args_parser.add_argument(
             "--num-iterations",
             type=int,
@@ -39,36 +40,22 @@ class AdventDay(Day.Base):
     def run(self):
         n = 0
         max_p = 0
-        cg = set()
-        groups = []
-        prices = []
+        cg_dict = {}
         #self.input = AdventDay.TEST_2
         for s in self._parse(self.input):
             x, p = self._next(s)
             n += x
-            prices.append(p)
-            g = self._change_groups(self._changes(p))
-            groups.append(g)
-            cg = cg | set(g)
-        #debug_print(f"N {n} NUM CH {len(cg)}")
-        #debug_print(f"ALL P {prices}")
-        gsz = len(groups[0][0])
-        debug_print(f"CHECK {len(cg)} GROUPS")
-        k = 0
-        for g in cg:
-            debug_if(f"CG {k}", k % 100 == 0)
-            p_sum = 0
-            for i, gg in enumerate(groups):
-                if g not in gg:
-                    #debug_print(f"{i} SKIP")
+            g = []
+            for j, ggg in enumerate(self._change_groups(p)):
+                if ggg in g:
                     continue
-                j = gg.index(g)
-                #debug_print(f"{i} FOUND {g} AT {j}/{len(gg)} CHECK P {gsz + j}/{len(prices[i])}")
-                p_sum += prices[i][gsz + j]
-                #debug_if(f"{i} P {prices[i][gsz + j - 1]}", g == (-2, 1, -1, 3))
-            debug_if(f"NEW SUM {p_sum} FOR {g}", p_sum > max_p)
-            max_p = max(max_p, p_sum)
-            k += 1
+                if ggg not in cg_dict:
+                    cg_dict[ggg] = 0
+                cg_dict[ggg] += p[j + self.change_group_size]
+                g.append(ggg)
+        max_p = max(cg_dict.values())
+        max_g = [x for x in cg_dict if cg_dict[x] == max_p]
+        #debug_print(f"CHECK {len(cg_dict)} GROUPS MAX? {max_p} G {max_g}")
         debug_print(f"MAX P {max_p}")
         return n
     
@@ -81,8 +68,9 @@ class AdventDay(Day.Base):
         return [prices[i + 1] - prices[i] for i in range(len(prices) - 2)]
     
 
-    def _change_groups(self, changes, group_size=4):
-        return [tuple(x) for x in [changes[i:i + group_size] for i in range(len(changes) - group_size + 1)]]
+    def _change_groups(self, prices):
+        changes = self._changes(prices)
+        return [tuple(x) for x in [changes[i:i + self.change_group_size] for i in range(len(changes) - self.change_group_size + 1)]]
 
 
     def _mix(self, val, secret):
@@ -101,8 +89,6 @@ class AdventDay(Day.Base):
             n = self._mix_prune(n // 32, n)
             n = self._mix_prune(2048 * n, n)
             p.append(self._price(n))
-        #c = self._changes(p)
-        #debug_print(f"P {p} CH {c} G {self._change_groups(c)}")
 
         return n, p
 
