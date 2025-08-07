771f6136116c909d96af6676f17fe82358836e4a
mkeilman <mvkeilman+github@gmail.com>
2025-08-07 09:36:36 -0600

    added include_time

--- utils/debug.py
+++ utils/debug.py
@@ -1,16 +1,20 @@
 """Debugging utilities
 """
 
-def debug_print(txt, **kawrgs):
+def debug_print(txt, include_time=False, **kwargs):
     """A wrapper to avoid "naked prints"
 
     Args:
         txt (any): something to print
     """
-    print(txt, **kawrgs)
+
+    import time
+    if include_time:
+        txt = f"[{int(time.time())}] " + txt
+    print(txt, **kwargs)
 
 
-def debug_if(txt, condition=None, **kwargs):
+def debug_if(txt, condition=None, include_time=False, **kwargs):
     if condition:
-        debug_print(txt, **kwargs)
+        debug_print(txt, include_time=include_time, **kwargs)
 
