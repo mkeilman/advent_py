898fc8cb666c0b36e74d0674e0f318165dccd5fb
mkeilman <mvkeilman+github@gmail.com>
2025-09-09 10:43:44 -0600

    2024 day 24 part 2 ckp

--- year_2024/Day_24.py
+++ year_2024/Day_24.py
@@ -150,33 +150,36 @@ class AdventDay(Day.Base):
         self.add_args(run_args)
         self.gates = []
         self.wires = {}
+        self.num_bits = 0
     
 
-    def anneal(self):
+    def solve(self):
+        self.reset()
         while self._wires_with_no_value():
-            for g in self._annealed_gates():
+            for g in self._solved_gates():
                 g.eval()
     
 
     def reset(self):
-        for w in self.wires:
+        for w in self.wires.values():
             w.reset()
 
 
     def run(self):
         #self.input = AdventDay.TEST_SWAPPED
         self._parse()
-        self.anneal()
-        xyz, sum = self._get_sum(6)
-        debug_print(f"X {xyz['x']} + Y {xyz['y']} -> Z {xyz['z']} VS SUM {sum}")
+        self.solve()
+        #b = self.num_bits
+        xyz, sum, mod_sum = self._get_sum()
+        #debug_print(f"X {xyz['x']} + Y {xyz['y']} -> Z {xyz['z']} VS SUM {sum}")
         if not self.validate_sum:
             return xyz["z"]
         debug_print(f"VALIDATE {len([x.name for x in self._get_outputs()])}")
-        self._validate(4)
+        self._validate()
         return 0
 
 
-    def _annealed_gates(self):
+    def _solved_gates(self):
         return [x for x in self.gates if x.in1.val is not None and x.in2.val is not None]
 
 
@@ -195,23 +198,24 @@ class AdventDay(Day.Base):
         return res
 
 
+    def _gates_for_bit(self, bit_num):
+        return self._gates_with_wire(f"z{bit_num:02}")
+
     def _gates_for_bits(self, n_bits):
         res = []
-        #res = {}
-        #tags = ("x", "y", "z")
-        #tags = ("z")
-        #for t in tags:
-        #    res[t] = []
         for i in range(n_bits):
-            res.extend(self._gates_with_wire(f"z{i:02}"))
+            res.extend(self._gates_for_bit(i))
         return res
 
 
     def _get_sum(self, max_bits=None):
         res = {}
+        mb = max_bits or self.num_bits
         for w in ("x", "y", "z"):
-            res[w] = self._tag_number(w, max_bits=max_bits)
-        return res, res["x"] + res["y"]
+            res[w] = self._tag_number(w, max_bits=mb)
+        #debug_print(f"{mb} BITS: SUM {res["x"] + res["y"]}; MOD SUM {(res["x"] + res["y"]) % (1 << mb)}")
+        return res, res["x"] + res["y"], (res["x"] + res["y"]) % (1 << mb)
+        #return res, res["x"] + res["y"]
 
 
     def _get_outputs(self):
@@ -247,12 +251,18 @@ class AdventDay(Day.Base):
                 self.gates.append(g)
                 for w in [x for x in g.wires if x.name not in self.wires]:
                     self.wires[w.name] = w
+        self.num_bits = len(self._tag_wires("z"))
     
 
     def _swap_vals(self, w1, w2):
         t = w1.val
         w1.val = w2.val
         w2.val = t
+
+    def _swap_outputs(self, gate1, gate2):
+        t = gate1.out
+        gate1.out = gate2.out
+        gate2.out = t
 
 
     def _tag_number(self, tag, max_bits=None):
@@ -268,35 +278,55 @@ class AdventDay(Day.Base):
         return sorted([v for k, v in self.wires.items() if k.startswith(tag)], key=lambda x: x.name)
     
 
-    def _validate(self, num_pairs):
+    def _validate(self):
         import math
         import itertools
+        import copy
 
-        #o = self._get_outputs()
-        # number of pairs
-        #n = math.comb(len(o), 2)
-        #oo = itertools.combinations(o, 2)
-        # number of pairs taken <num_pairs> at a time
-        #m = math.comb(n, num_pairs)
-        #p = math.comb(len(o), 2 * num_pairs)
-        #q = math.comb(p, 2)
-        #debug_print(f"NUM OUTPUTS: {len(o)}; NUM PAIRS: {n}; NUM OF {num_pairs} PAIRS: {m}; NUM GROUPS OF {2 * num_pairs}: {p}; Q {q}")
-        #xyz, sum = self._get_sum()
-        #while sum != xyz["x"] + xyz["y"]:
-        b = 1
-        gates = self._gates_for_bits(b)
-        s = set(gates)
-        #debug_print(f"B {b} GATES {[x.to_string() for x in gates]}")
-        for g in gates:
-            debug_print(f"B {b} G {g}")
-            for w in g.wires:
-                gg = self._gates_with_wire(w.name)
-                s = s | set(gg)
-                #gg = self._gates_connected_to(w.name)
-                debug_print(f"B {b} GG {gg}")
-        #debug_print(f"B {b} SET {s}")
-        outs = [x.out for x in s]
-        debug_print(f"B {b} SET OUT {outs}")
+        for b in range(1, self.num_bits):
+            #b = 1
+            total_swaps = 4
+            n_swaps = 0
+            xyz, sum, mod_sum = self._get_sum(b)
+            zn = self._tag_number("z", max_bits=b)
+            if mod_sum == zn:
+                continue
+            if n_swaps == total_swaps:
+                break
+            debug_print(f"INVALID SUM Z B {b} X {xyz['x']} + Y {xyz['y']} {zn} != {mod_sum} ({sum})")
+            gates = self._gates_for_bits(b)
+            gg = self._gates_for_bit(b)
+            #s = set(gates)
+            s = set(gg)
+            debug_print(f"B {b} GATES {gg}")
+            #for g in gates:
+            for g in gg:
+                #debug_print(f"B {b} G {g}")
+                for w in g.wires:
+                    ggg = self._gates_with_wire(w.name)
+                    s = s | set(ggg)
+                    #gg = self._gates_connected_to(w.name)
+                    debug_print(f"B {b} WIRE {w} GG {ggg}")
+            debug_print(f"B {b} SET {s}")
+            outs = [x.out for x in s]
+            debug_print(f"B {b}/{self.num_bits} SET OUT {outs}")
+            for p in itertools.combinations(s, 2):
+                if p[0].out in p[1].inputs or p[1].out in p[0].inputs:
+                    debug_print(f"CANNOT SWAP {p}")
+                    continue
+                debug_print(f"SWAP {p}")
+                self._swap_outputs(p[0], p[1])
+                self.solve()
+                zyx, smm, mmssmm = self._get_sum(b)
+                zznn = self._tag_number("z", max_bits=b)
+                debug_print(f"NEW SUM Z B {b} X {zyx['x']} + Y {zyx['y']} {zznn} VS {mmssmm} ({smm})")
+                if mod_sum == zn:
+                    n_swaps += 1
+                    break
+                #put them back
+                self._swap_outputs(p[0], p[1])
+            break
+            
         
 
     def _wire_names(self):
@@ -308,4 +338,4 @@ class AdventDay(Day.Base):
     
 
     def _wires_with_no_value(self):
-        return [v for k, v in self.wires.items() if v.val is None]
+        return [w for w in self.wires.values() if w.val is None]
