0d82f1acf2f46e1509cdc56f8453a572736b527d
mkeilman <mvkeilman+github@gmail.com>
2025-10-03 19:40:31 -0600

    2024 25 part 1

--- year_2024/Day_25.py
+++ year_2024/Day_25.py
@@ -9,12 +9,16 @@ class Schematic:
     FILLED = "#"
     EMPTY = "."
 
+    TYPES = ["KEY", "LOCK"]
+
     def __init__(self, grid):
         self.num_rows = len(grid)
         self.num_cols = len(grid[0])
+        self.channel_length = self.num_rows - 2
 
         self.is_lock = grid[0] == Schematic.FILLED * self.num_cols and grid[self.num_rows - 1] == Schematic.EMPTY * self.num_cols
         self.is_key = grid[self.num_rows - 1] == Schematic.FILLED * self.num_cols and grid[0] == Schematic.EMPTY * self.num_cols
+        self.s_type = Schematic.TYPES[1 - self.is_lock]
 
         self.cols = self.num_cols * [""]
         for line in grid:
@@ -24,7 +28,19 @@ class Schematic:
         self.lengths = []
         for c in self.cols:
             self.lengths.append(len([x for x in c if x == Schematic.FILLED]) - 1)
-        debug_print(f"COLS {self.cols} LENS {self.lengths}")
+
+
+    def __repr__(self):
+        return f"{self.cols}"
+    
+
+    def fit(self, other_schematic):
+        assert self.num_rows == other_schematic.num_rows and self.num_cols == other_schematic.num_cols
+        if self.s_type == other_schematic.s_type:
+            return False
+        sums = [x + other_schematic.lengths[i] for i, x in enumerate(self.lengths)]
+        #debug_print(f"S1 {self} S2 {other_schematic}: {sums}")
+        return all([x <= self.channel_length for x in sums])
 
 
 class Lock(Schematic):
@@ -101,11 +117,21 @@ class AdventDay(Day.Base):
 
     def run(self):
         debug_print("RUN")
-        self.schematics = []
-        self.locks = []
-        self.keys = []
         self._parse()
+        #debug_print(f"K {self.keys} L {self.locks}")
+        n = self._count_fits()
+        debug_print(f"FITS {n}")
         return 0
+
+
+    def _count_fits(self):
+        n = 0
+        #debug_print(f"NK {len(self.keys)} NL {len(self.locks)}")
+        #return 0
+        for k in self.keys:
+            for l in self.locks:
+                n += k.fit(l)
+        return n
 
 
     def _parse(self):
@@ -117,6 +143,9 @@ class AdventDay(Day.Base):
             else:
                 self.locks.append(sx)
 
+        self.schematics = []
+        self.locks = []
+        self.keys = []
         s = []
         for line in self.input:
             if not line:
