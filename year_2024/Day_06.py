f8cf8b9c0f611f6389fdfab786d71123d3fee245
mkeilman <mvkeilman+github@gmail.com>
2025-10-05 08:26:44 -0600

    2024 06 ckp

--- year_2024/Day_06.py
+++ year_2024/Day_06.py
@@ -2,9 +2,11 @@ import re
 import Day
 from utils import mathutils
 from utils import string
-from utils.debug import debug_print
+from utils.debug import debug_print, debug_if
 
 class Room:
+
+    WALL = "#"
 
     #@classmethod
     #def is_loop(cls, arr):
@@ -15,6 +17,7 @@ class Room:
         self.size = (len(grid), len(grid[0]))
 
 
+    # a path is an array (position, direction)
     def find_path(self, start_pos, start_dir):
 
         def _is_in_room(pos):
@@ -30,13 +33,13 @@ class Room:
 
             if dir[1] > 0:
                 try:
-                    j = s.index("#", pos[0])
+                    j = s.index(Room.WALL, pos[0])
                 except  ValueError:
                     j = len(self.grid[0])
                 return s[pos[1]:j + 1]
             #if dir[1] < 0:
             #    try:
-            #        j = s.index("#", pos[0])
+            #        j = s.index(Room.WALL, pos[0])
             #    except  ValueError:
             #        j = 0
             #    return s[pos[1]:j + 1]
@@ -52,7 +55,8 @@ class Room:
             #debug_print(f"CHECK STR {_sub_string(pos, dir)}")
             q = (pos[0] + dir[0], pos[1] + dir[1])
             if _is_in_room(q):
-                if self.grid[q[0]][q[1]] == "#":
+                if self.grid[q[0]][q[1]] == Room.WALL:
+                    # ran into a wall, so try moving in the next direction
                     dir = d[(d.index(dir) + 1) % len(d)]
                     p.append((pos, dir))
                     continue
@@ -71,7 +75,22 @@ class Room:
     
 
     def is_loop(self, arr):
+        #if arr[0] == arr[-1]:
+        #    debug_print(f"ARR0 {arr[0]} VS ARRN {arr[-1]} LENS? {len(arr) > len(set(arr))}")
         return len(arr) > len(set(arr))
+    
+
+    def print_path(self, path):
+        dirs = {v: k for k, v in Guard.DIRECTIONS.items()}
+        for i, r in enumerate(self.grid):
+            l = ""
+            e = [x[0] for x in path if x[0][0] == i]
+            d = [x[1] for x in path if x[0][0] == i]
+            for j, c in enumerate(r):
+                l += dirs[d[e.index((i, j))]] if (i, j) in e else "."
+            debug_print(l)
+                
+
 
 class Guard:
 
@@ -100,6 +119,7 @@ class Guard:
         self.direction = self.init_dir
 
 
+
 class AdventDay(Day.Base):
             
     TEST = [
@@ -118,6 +138,7 @@ class AdventDay(Day.Base):
     def __init__(self, run_args):
         import argparse
         super(AdventDay, self).__init__(2024, 6)
+
 
     def _get_guard(self, v):
         d = fr"[{'|'.join(Guard.RE_DIRS.values())}]"
@@ -143,34 +164,38 @@ class AdventDay(Day.Base):
         # only need to consider spaces on the original path?
         #for space in set([x[0] for x in init_path[1:]]):
         for k in range(1, len(init_path)):
+            #debug_if(f"K {k}/{len(init_path)}", condition=k % 100 == 1, include_time=True)
             space = init_path[k]
             prev_space = init_path[k - 1]
-            i = space[0][0]
-            j = space[0][1]
+            i, j = space[0]
             # already placed a barrier here
             if (i, j) in o:
                 continue
             g = grid[:]
-            g[i] = g[i][:j] + "#" + g[i][j + 1:]
+            g[i] = g[i][:j] + Room.WALL + g[i][j + 1:]
             #p = Room(g).find_path(pos, dir)
             # start at the previous space
             r = Room(g)
-            p = r.find_path(prev_space[0], prev_space[1])
-            if r.is_loop(p):
-                debug_print(f"{k} {i} {j}")
-                #n += 1
-                o.append((i, j))
-                l = r.get_loop(p)
-                loops = loops.union(set(l))
-                debug_print(f"L {len(l)} N L {len(loops)}")
-                #pp = pp.union(set(sorted([_multiplex(x[0], x[1]) for x in p])))
-                #m = sorted([_multiplex(x[0], x[1]) for x in p])
-                #if m not in pp:
-                #    pp.append(m)
-                #o = o.union(set(sorted(sorted(p, key=lambda x: x[0][0]), key=lambda x: x[0][1])))
+            p = r.find_path(*prev_space)
+            if not r.is_loop(p):
+                continue
+            #debug_print(f"{k} {i} {j}")
+            #n += 1
+            o.append((i, j))
+            l = r.get_loop(p)
+            #r.print_path(l)
+            #debug_if(f"K {k}/{len(init_path)} LOOP {l}", condition=True, include_time=True)
+            #loops = loops.union(set(l))
+            loops = loops | set(l)
+            #debug_print(f"L {len(l)} N L {len(loops)}")
+            #pp = pp.union(set(sorted([_multiplex(x[0], x[1]) for x in p])))
+            #m = sorted([_multiplex(x[0], x[1]) for x in p])
+            #if m not in pp:
+            #    pp.append(m)
+            #o = o.union(set(sorted(sorted(p, key=lambda x: x[0][0]), key=lambda x: x[0][1])))
 
 
-        debug_print(f"LOOPS UNIQUE? {loops}")
+        #debug_print(f"LOOPS UNIQUE? {loops}")
         #return n
         return len(o)
             
