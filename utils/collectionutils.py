8f74465179c8b6d13c9ea2fdda95169b107ed5be
mkeilman <mvkeilman+github@gmail.com>
2025-11-11 13:34:50 -0700

    ckp

--- utils/collectionutils.py
+++ utils/collectionutils.py
@@ -38,48 +38,30 @@ def random_exchanges(elements, exclusions=None):
 
     excl = exclusions or []
 
-    # there are n(n - 1) possible exchanges, and each exclusion removes 2, so
+    # there are n(n - 1) / 2 possible exchanges, and each exclusion removes 2, so
     # check if enough remain to pair (at least n pairings)
-    # treat (a, b) and (b, a) as the same combination?
     if (n * (n - 1) // 2) - 2 * len(excl) < n:
         raise ValueError(f"can exclude no more than {n * (n - 2) // 2} pairs")
 
-
-    exchanges = []
-    # note this considers (a, b) and (b, a) as the same combination, but we do not
-    all_pairs = itertools.combinations(elements, 2)
-    #debug_print(list(all_pairs))
     # randomize the elements so any forced assignment is not always the same
     random.shuffle(elements)
-    # accomodate the exclusions first
-    pairs_less_exclusions = [x for x in all_pairs if not _has_exclusion(*x)]
-    #if len(pairs_less_exclusions) < n:
-    #    raise ValueError(f"")
     
-    #debug_print(pairs_less_exclusions)
+    exchanges = []
 
+    # unique excluded elements
     excluded_elements = list(set(flatten(excl)))
-    el_less_excl = [x for x in elements if x not in excluded_elements]
-
-
-
-    #for i, e in enumerate(elements):
-    eeee = excluded_elements + el_less_excl
+    # accomodate the exclusions first
+    eeee = excluded_elements + [x for x in elements if x not in excluded_elements]
     for i, e in enumerate(eeee):
         v = _valid_exchange(e, exchanges)
-        #debug_print(f"VALID FOR {e} OUT OF {eeee}: {v}")
 
         # if we're down to the last 2 elements and the last element has not
         # already been selected, select it now - otherwise it will not be paired
         if i == len(eeee) - 2 and eeee[-1] not in exchanges:
-            debug_print(f"{e} MUST CHOOSE {eeee[-1]}")
             exchanges.append(eeee[-1])
-            debug_print(f"{_exchanges(eeee, exchanges)[-1]}")
             continue
 
-        x = random.choice(v)
-        exchanges.append(x)
-        #debug_print(f"{_exchanges(eeee, exchanges)[-1]}")
+        exchanges.append(random.choice(v))
 
     return _exchanges(eeee, exchanges)
 
@@ -87,7 +69,7 @@ def random_exchanges(elements, exclusions=None):
 def main():
     e = ["Mike", "John", "Joe", "Thomb", "Patrick", "Jerry", "Sari", "Erin"]
     ex = [("Jerry", "Sari"), ("Joe", "Erin")]
-    #e = [1, 2, 3, 4]
+    #e = [1, 2, 3]
     #ex = [(1, 2)]
     debug_print(random_exchanges(e, exclusions=ex))
     #x = random_exchanges(e)
