1f073ffda798d0597d040ecd9f2703e37a1b21cb
mkeilman <mvkeilman+github@gmail.com>
2025-12-01 10:09:04 -0700

    2025 day 2

--- year_2025/Day_01.py
+++ year_2025/Day_01.py
@@ -12,16 +12,26 @@ class Dial:
 
     
     def turn(self, num_spaces):
-        self.current_pos = (self.current_pos + self.num_digits + num_spaces) % self.num_digits
-        #debug_print(f"CURRENT POS {self.current_pos}")
+        # pre-modulo
+        pos = self.current_pos + num_spaces
+        # don't count interim 0 if starting at
+        interim = int(self.current_pos and (pos < 0 or pos > self.num_digits))
+        n = abs(num_spaces) // self.num_digits
+        if n:
+            interim += (n - 1)
+        #debug_print(f"PREMOD {self.current_pos} -> {pos} NUM FULL ROT {n} INT 0 {interim}")
+        self.current_pos = (pos + self.num_digits) % self.num_digits
+        return interim
 
 
-
-    def spin(self, turns):
+    def spin(self, turns, count_interim_zeros=False):
         num_zeros = 0
         for t in turns:
-            self.turn(t)
+            int_zeros = self.turn(t)
+            # lands on 0
             num_zeros += not self.current_pos
+            if count_interim_zeros:
+                num_zeros += int_zeros
         return num_zeros
 
 
@@ -40,25 +50,29 @@ class AdventDay(Day.Base):
         "L82",
     ]
 
+    BIG_ROT = [
+        "L1000",
+    ]
+
 
     def __init__(self, run_args):
+        import argparse
         super(AdventDay, self).__init__(2025, 1)
-        #self.args_parser.add_argument(
-        #    "--calc",
-        #    type=str,
-        #    help="calculation",
-        #    choices=["col-diffs", "similarity"],
-        #    default="col-diffs",
-        #    dest="calc",
-        #)
-        #self.add_args(run_args)
+        self.args_parser.add_argument(
+            "--count-interim-zeros",
+            action=argparse.BooleanOptionalAction,
+            default=False,
+            dest="count_interim_zeros",
+        )
+        self.add_args(run_args)
 
 
     def run(self):
         d = Dial()
         turns = [self._parse_turn(x) for x in self.input]
-        n = d.spin(turns)
-        debug_print(f"FINAL POS {d.current_pos} NUM 0 {n}")
+        n = d.spin(turns, count_interim_zeros=self.count_interim_zeros)
+        debug_print(f"FINAL POS {d.current_pos} NUM ZEROS {n}")
+        return n
  
 
     
