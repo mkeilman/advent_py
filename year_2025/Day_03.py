a462f085e22943f8163f560caf8b0a1b1cb54f24
mkeilman <mvkeilman+github@gmail.com>
2025-12-03 13:11:30 -0700

    2025 day 3 part 2 in progress

--- year_2025/Day_03.py
+++ year_2025/Day_03.py
@@ -14,28 +14,44 @@ class AdventDay(Day.Base):
     def __init__(self, run_args):
         import argparse
         super(AdventDay, self).__init__(2025, 3)
+        self.args_parser.add_argument(
+            "--num-batts",
+            type=int,
+            help="number of batteries",
+            default=2,
+            dest="num_batts",
+        )
         self.add_args(run_args)
 
 
     def run(self):
-        n = self._max_joltage_sum(num_batts=2)
+        n = self._max_joltage_sum()
         debug_print(f"RUN {self.year} {self.day}: {n}")
         return n
  
 
-    def _max_joltage(self, bank, num_batts=2):
+    def _max_joltage(self, bank):
         i = 0
         last_i = 0
         j = ""
         k = len(bank)
         n = 0
-        while n < num_batts:
-            #debug_print(f"CHECK {bank[i:k]}")
-            i = bank.index(str(max([int(x) for x in bank[i:k]])))
+        while n < self.num_batts:
+            b = bank[i:k]
+            debug_print(f"CHECK {b}")
+            # start over?
+            #if not b:
+            #    i = 0
+            #    last_i = 0
+            #    k -= 1
+            #    j = ""
+            #    n = 0
+            #    continue
+            i = bank.index(str(max([int(x) for x in b])))
             # if we still have batteries to connect but have reached the
-            # end of the bank, start again
-            if n < num_batts - 1 and i == len(bank) - 1:
-                #debug_print(f"BANK {bank} J {bank[i]} AT {i} NO MORE BATTS")
+            # end of the bank, start again excluding the last battery
+            if n < self.num_batts - 1 and i == len(bank) - 1:
+                debug_print(f"BANK {bank} J {bank[i]} AT {i} NO MORE BATTS")
                 k -= 1
                 i = last_i
                 continue
@@ -50,8 +66,8 @@ class AdventDay(Day.Base):
         return int(j)
 
 
-    def _max_joltage_sum(self, num_batts=2):
+    def _max_joltage_sum(self):
         j = 0
         for bank in self.input:
-            j += self._max_joltage(bank, num_batts=num_batts)
+            j += self._max_joltage(bank)
         return j
