aa9592d5647da58d5bd42722581bd98b92f25f28
mkeilman <mvkeilman+github@gmail.com>
2025-12-03 10:02:22 -0700

    2025 day 3 part 1

--- year_2025/Day_03.py
+++ year_2025/Day_03.py
@@ -4,6 +4,10 @@ from utils.debug import debug_print, debug_if
 class AdventDay(Day.Base):
 
     TEST = [
+        "987654321111111",
+        "811111111111119",
+        "234234234234278",
+        "818181911112111",
     ]
 
 
@@ -14,7 +18,40 @@ class AdventDay(Day.Base):
 
 
     def run(self):
-        n = 0
+        n = self._max_joltage_sum(num_batts=2)
         debug_print(f"RUN {self.year} {self.day}: {n}")
         return n
  
+
+    def _max_joltage(self, bank, num_batts=2):
+        i = 0
+        last_i = 0
+        j = ""
+        k = len(bank)
+        n = 0
+        while n < num_batts:
+            #debug_print(f"CHECK {bank[i:k]}")
+            i = bank.index(str(max([int(x) for x in bank[i:k]])))
+            # if we still have batteries to connect but have reached the
+            # end of the bank, start again
+            if n < num_batts - 1 and i == len(bank) - 1:
+                #debug_print(f"BANK {bank} J {bank[i]} AT {i} NO MORE BATTS")
+                k -= 1
+                i = last_i
+                continue
+            j += bank[i]
+            #debug_print(f"BANK {bank} J {j} AT {i}")
+            i += 1
+            last_i = i
+            n += 1
+            k = len(bank)
+
+        debug_print(f"BANK {bank} J {j}")
+        return int(j)
+
+
+    def _max_joltage_sum(self, num_batts=2):
+        j = 0
+        for bank in self.input:
+            j += self._max_joltage(bank, num_batts=num_batts)
+        return j
