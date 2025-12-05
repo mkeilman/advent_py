fa0c8aa13bc4e38a96eef24b1cb9839101858250
mkeilman <mvkeilman+github@gmail.com>
2025-12-04 21:14:26 -0700

    2025 day 3 part 2 ckp

--- year_2025/Day_03.py
+++ year_2025/Day_03.py
@@ -1,5 +1,7 @@
 import Day
 from utils.debug import debug_print, debug_if
+from utils import collectionutils
+from utils import stringutils
 
 class AdventDay(Day.Base):
 
@@ -30,7 +32,7 @@ class AdventDay(Day.Base):
 
 
     def run(self):
-        self.input = AdventDay.SHORT_BANK
+        #self.input = AdventDay.SHORT_BANK
         n = self._max_joltage_sum()
         debug_print(f"RUN {self.year} {self.day}: {n}")
         return n
@@ -40,13 +42,56 @@ class AdventDay(Day.Base):
         if self.num_batts > len(bank):
             raise ValueError(f"too many batteries: {self.num_batts} > {len(bank)}")
         
+        if self.num_batts < 2:
+            raise ValueError(f"too few batteries: {self.num_batts} < 2")
+        
         i = 0
         last_i = 0
-        j = ""
+        joltage = ""
         k = len(bank)
         n = 0
+        sorted_bank = sorted(bank, reverse=True)
+        inds = []
+        for batt in sorted_bank:
+            inds.extend([x for x in stringutils.indices(batt, bank) if x not in inds])
+       
+        debug_print(f"BANK {bank} SORTED {sorted_bank} INDS {inds}")
+        start = 0
+        digits = []
+        i = 0
+        #while not done:
+        while len(digits) < self.num_batts:
+            j = inds[start + i]
+            # extract j?
+            ii = [j] + [x for x in inds if x != j]
+            i += 1
+            # next digit is before the current
+            #debug_print(f"CHECK INDS {ii[i]} VS J {j}")
+            if inds[start + i] < j:
+            #if ii[i] < j:
+                debug_print(f"START OVER")
+                digits = []
+                start += 1
+                i = 0
+                continue
+            debug_print(f"ADD IDX {j}")
+            digits.append(bank[j])
+            debug_print(f"DIGITS NOW {digits}")
+        joltage = "".join(digits)
+
+
+        #digits = sorted_bank[-self.num_batts:]
+        #digit_inds = inds[-self.num_batts:]
+        ##sorted_inds = sorted(digit_inds)
+        #debug_print(f"DIGITS {digits} DIGIT INDS {digit_inds} SORTED INDS {sorted_inds}")
+        #j = ""
+        #for ii in sorted_inds:
+        #    j += bank[ii]
+        debug_print(f"MAX J ? {joltage}")
+        return int(joltage)
+    
         #while n < self.num_batts:
-        while len(j) < self.num_batts:
+        while len(joltage) < self.num_batts:
             b = bank[i:k]
             debug_print(f"CHECK {b} I {i} -> K {k}")
             # no more batts
@@ -56,21 +101,21 @@ class AdventDay(Day.Base):
                 i = 0
                 last_i = 0
                 k -= 1
-                j = ""
+                joltage = ""
             #    n = 0
                 continue
             i = bank.index(str(max([int(x) for x in b])))
             # if we still have batteries to connect but have reached the
             # end of the bank, start again excluding the last battery
             #if n < self.num_batts - 1 and i == len(bank) - 1:
-            if len(j) < self.num_batts - 1 and i == len(bank) - 1:
+            if len(joltage) < self.num_batts - 1 and i == len(bank) - 1:
                 debug_print(f"BANK {bank} J {bank[i]} AT {i} NO MORE BATTS")
                 k -= 1
                 i = last_i
-                #j = j[:-1]
+                #joltage = joltage[:-1]
                 continue
-            j += bank[i]
-            debug_print(f"J {j} AT {i}")
+            joltage += bank[i]
+            debug_print(f"J {joltage} AT {i}")
             # search for next digit after the one we found
             i += 1
             last_i = i
@@ -78,8 +123,8 @@ class AdventDay(Day.Base):
             # reset to check to the end of the bank?
             k = len(bank)
 
-        debug_print(f"BANK {bank} J {j}")
-        return int(j)
+        debug_print(f"BANK {bank} J {joltage}")
+        return int(joltage)
 
 
     def _max_joltage_sum(self):
