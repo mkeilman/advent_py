af0a147a21b02ac16b629ba274cdf99836872c17
mkeilman <mvkeilman+github@gmail.com>
2025-12-07 09:20:22 -0700

    2025 day 6 part 2 ckp

--- year_2025/Day_06.py
+++ year_2025/Day_06.py
@@ -7,11 +7,13 @@ from utils import mathutils
 class AdventDay(Day.Base):
 
     TEST = [
-        "123 328  51 64" ,
-        "45 64  387 23" ,
-        "6 98  215 314 ",
+        "123 328  51 64 ",
+        " 45 64  387 23 " ,
+        "  6 98  215 314",
         "*   +   *   + ",
     ]
+
+    
 
     OPS = {
         "+": {
@@ -32,31 +34,68 @@ class AdventDay(Day.Base):
 
 
     def run(self):
-        self.nuns = []
+        self.nums = []
+        self.txt = []
+        self.num_cols = []
         self.ops = []
         self.results = []
+        self.magnitudes = []
+        self.magnitude = pow(10, len(self.input) - 2)
         n = 0
         self._parse()
         self._apply_ops()
         #debug_print(f"OPS {self.ops} NUMS {self.nums} RES {self.results}")
         n = mathutils.sum(self.results)
-        debug_print(f"RES SUM {n}")
+        #debug_print(f"RES SUM {n} NUM COLS {self.num_cols}")
         return n
     
+
+    def _apply_ops(self):
+        for r in self.nums:
+            for i, n in enumerate(r):
+                self.results[i] = self.ops[i](self.results[i], n)
+    
+
     def _parse(self):
         self.nums = []
         for line in self.input[:-1]:
             self.nums.append([int(x) for x in re.findall(r"\d+", line)])
+            self.txt.append(re.findall(r"\s*\d+\s?", line))
+        
+        #debug_print(f"TXT {self.txt}")
+        self.magnitudes = len(self.nums[0]) * [0]
+        self.num_cols = [[] for _ in range(len(self.nums[0]))]
+        #self.num_cols = ["" for _ in range(len(self.nums[0]))]
+
+        for i, r in enumerate(self.nums):
+            for j, n in enumerate(r):
+                self.magnitudes[j] = max(self.magnitudes[j], len(str(n)) - 1)
+                #self.num_cols[j].append(self.input[i][j])
+                #self.num_cols[j] = self.input[i][j]
+                self.num_cols[j].append(self.txt[i][j])
+
+        #self.num_cols.reverse()
+        #for i, c in enumerate(self.num_cols):
+        #    if all([x[0] == " " for x in c]):
+        #        self.num_cols[i] = [x[1:] for x in c]
+        for i, c in enumerate(self.num_cols):
+            if all([x[-1] == " " for x in c]):
+                self.num_cols[i] = [x[:-1] for x in c]
+            debug_print(f"CEPH {self._to_ceph(c)}")
+        debug_print(f"NUM COLS {self.num_cols}")
 
         k = ["\\" + x for x in AdventDay.OPS.keys()]
         re_ops = fr"[{'|'.join(k)}]"
         op_keys = re.findall(re_ops, self.input[-1])
         self.ops = [AdventDay.OPS[x]["op"] for x in op_keys]
         self.results = [AdventDay.OPS[x]["unit"] for x in op_keys]
- 
 
-    def _apply_ops(self):
-        for r in self.nums:
-            for i, n in enumerate(r):
-                self.results[i] = self.ops[i](self.results[i], n)
 
+    
+    def _to_ceph(self, num_strings):
+        ceph = len(num_strings) * [""]
+        max_len = max([len(str(int(x))) for x in num_strings])
+        for j in range(max_len):
+            for s in num_strings:
+                ceph[j] += s[j]
+        return ceph
