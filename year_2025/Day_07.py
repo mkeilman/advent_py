85a62524990c83ba5d52c8b805074da204295a81
mkeilman <mvkeilman+github@gmail.com>
2025-12-08 08:09:15 -0700

    2025 day 7 part 2 ckp

--- year_2025/Day_07.py
+++ year_2025/Day_07.py
@@ -21,7 +21,7 @@ class AdventDay(Day.Base):
         "..^...^.....^..",
         "...............",
         ".^.^.^.^.^...^.",
-        "...............",
+        #"...............",
     ]
 
     ENTRY = "S"
@@ -33,26 +33,40 @@ class AdventDay(Day.Base):
         import argparse
         super(AdventDay, self).__init__(2025, 7)
         self.add_args(run_args)
-        self.grid = None
 
 
     def run(self):
         n = 0
-        self._parse()
         self.entry_pos = self._token_pos(AdventDay.ENTRY)
         self.splitter_pos = self._token_positions(AdventDay.SPLITTER)
         #debug_print(f"ENTRY {self.entry_pos} S {self.splitter_pos}")
-        self._propagate()
+        n, all_pos = self._propagate()
+        debug_print(f"NUM SPLITS {n}")
+        m = self._num_paths(all_pos)
+        debug_print(f"NUM SPLITS {m}")
         return n
  
 
-    def _parse(self):
-        self.grid = Day.Grid.grid_of_size(len(self.input), len(self.input[0]))
+    def _num_paths(self, beam_positons, depth=0):
+        n = 0
+        #debug_print(f"{depth} GET P FROM {beam_positons}")
+        for i in range(len(beam_positons) - 1):
+            b1 = beam_positons[i]
+            b2 = beam_positons[i + 1]
+            debug_print(f"{depth} GET P FROM {b1} - {b2}")
+            for r in b1:
+                #debug_print(r)
+                next = [x for x in b2 if abs(x[1] - r[1]) == 1]
+                debug_print(f"{depth} NEXT {next}")
+                n += len(next)
+        return n
 
 
     def _propagate(self):
         def _next_pos(bp, row):
-            n = 0
+            ns = 0
+            np = 0
+
             splitter_pos = [(i, j) for j in stringutils.indices(AdventDay.SPLITTER, list(row))]
             next_pos = set()
             for p in bp:
@@ -62,21 +76,22 @@ class AdventDay(Day.Base):
                 else:
                     next_pos.add((i, p[1] + 1))
                     next_pos.add((i, p[1] - 1))
-                    n += 1
-            #debug_print(f"N S {n}")
-            return n, next_pos
-            
+                    ns += 1
+                    np += 2
+            debug_print(ns)
+            return np, ns, next_pos
 
         num_splits = 0
+        num_paths = 0
         beam_positions = set([self.entry_pos])
+        all_pos = []
         for i, r in enumerate(self.input):
-            m, beam_positions = _next_pos(beam_positions, r)
+            n, m, beam_positions = _next_pos(beam_positions, r)
+            all_pos.append(beam_positions)
             num_splits += m
-            #debug_print(f"BP {beam_positions}")
-        debug_print(f"NUM SPLITS {num_splits}")
-                
-            
-
+            num_paths += n
+        #debug_print(f"NP {num_paths}")
+        return num_splits, all_pos
 
 
     def _token_pos(self, token):
