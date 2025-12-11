9c640b918480a8042accffa0012ff9e2bf026755
mkeilman <mvkeilman+github@gmail.com>
2025-12-11 14:46:20 -0700

    2025 day 11 part 2 ckp

--- year_2025/Day_11.py
+++ year_2025/Day_11.py
@@ -34,37 +34,60 @@ class AdventDay(Day.Base):
         "hhh: out",
     ]
 
-    YOU = "you"
-    OUT = "out"
-    SERVER = "svr"
+    DAC = "dac"
     FFT = "fft"
+    OUT = "out"
+    SVR = "svr"
+    YOU = "you"
+    
 
 
     def __init__(self, run_args):
         import argparse
         super(AdventDay, self).__init__(2025, 11)
+        self.args_parser.add_argument(
+            "--count-dac-fft",
+            action=argparse.BooleanOptionalAction,
+            default=False,
+            dest="count_dac_fft",
+        )
         self.add_args(run_args)
         self.rack = {}
 
 
     def run(self):
         n = 0
+        if self.mode == "test" and self.count_dac_fft:
+            self.input = AdventDay.SERVER
         self._parse()
-        #debug_print(f"R {self.rack}")
-        n = self._num_paths()
+        #n = self._num_paths()
+        self._ancestors(AdventDay.DAC)
         debug_print(f"N {n}")
         return n
  
 
+    def _ancestors(self, device, chain=None, depth=0):
+        c = chain or []
+        c.append(device)
+        parents = [x for x in self.rack if device in self.rack[x]]
+        for p in parents:
+            self._ancestors(p, chain=c, depth=depth+1)
+        debug_if(f"{depth} {c}", "", "", depth == 0)
+
+
     def _num_paths(self, key=None, depth=0, dev_chain=None):
         n = 0
-        k = key or AdventDay.YOU
-        c = dev_chain or f"{k}:"
-        #debug_print(f"{depth} KEY {k}")
+        k = key or (AdventDay.SVR if self.count_dac_fft else AdventDay.YOU)
+        c = dev_chain or f"{k}"
         for d in self.rack[k]:
             dl = f"{c}:{d}"
+            debug_if(f"{depth} {dl}", "", "", depth == 0)
             if d == AdventDay.OUT:
-                #debug_print(f"{depth} {dl}")
+                debug_print(f"{depth} {dl}")
+                if self.count_dac_fft:
+                    #if AdventDay.DAC in dl and AdventDay.FFT in dl:
+                    #    debug_print(f"{depth} {dl}")
+                    return int(AdventDay.DAC in dl and AdventDay.FFT in dl)
                 return 1
             else:
                 n += self._num_paths(key=d, depth=depth+1, dev_chain=dl)
@@ -76,3 +99,5 @@ class AdventDay(Day.Base):
         for line in self.input:
             d = re.findall(r"[a-z]{3}", line)
             self.rack[d[0]] = d[1:]
+
+        v = self.rack.values()
