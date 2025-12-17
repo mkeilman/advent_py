873debaf75ff53db872aec2579dca45ea4c6101d
mkeilman <mvkeilman+github@gmail.com>
2025-12-16 19:47:36 -0700

    2025 day 3 part 2 ckp

--- year_2025/Day_03.py
+++ year_2025/Day_03.py
@@ -48,31 +48,18 @@ class AdventDay(Day.Base):
         return  sorted([(i, bank[i]) for i in inds], reverse=True, key=operator.itemgetter(1))
 
 
-    def _max_joltage_list(self, batt_index, num_batts=None, max_index=0, depth=0):
+    def _max_joltage_list(self, batt_index, num_batts=None, depth=0):
         import operator
         
         nb = num_batts or self.num_batts
-        # the number as written is the largest it can be
-        #if nb == len(bank):
-        #    return "".join(bank)
         
-        i = 0
-
-        #batt_index = self._bank_to_batt_index(bank)
-       
-        #debug_print(f"{depth} BANK {batt_index} NB {nb}")
         start = 0
-        digits = []
-        i = 0
-        #sbi = sorted(batt_index, reverse=True, key=operator.itemgetter(1))
-        #debug_print(f"{depth} SBI {sbi}")
         i, d = batt_index[start]
-        digits.append(d)
+        digits = [d]
         while len(digits) < nb:
-            #debug_print(f"NEXT {start}")
             # get the remaining digits whose index is greater than the current digit's index
             remainder = [x for x in batt_index if x[0] > i]
-            #debug_print(f"{depth} D {digits} R {remainder}")
+
             total = len(digits) + len(remainder)
             # if the number of digits plus the number remaining is num_batts, add to the 
             # remainder sorted by index - we're done
@@ -85,13 +72,8 @@ class AdventDay(Day.Base):
                 start += 1
                 i, d = batt_index[start]
                 digits = [d]
-                #debug_print(f"{depth} START OVER AT {start}")
                 continue
 
-            #remove_idx = i
-            #debug_print(f"{depth} REMOVE {d} AT {i}")
-            #i, d = remainder[0]
-            #debug_print(f"{depth} NEXT DGIT OK {i} {d}")
             digits.extend(self._max_joltage_list(remainder, num_batts=nb - 1, depth=depth + 1))
 
         return digits
@@ -106,74 +88,8 @@ class AdventDay(Day.Base):
         if self.num_batts < 2:
             raise ValueError(f"too few batteries: {self.num_batts} < 2")
         
-        #debug_print(f"GET J FOR {bank}")
         batt_index = self._bank_to_batt_index(bank)
-
-        js = self._max_joltage_list(batt_index)
-        #debug_print(f"MAX J ? {int("".join(js))}")
-        return int("".join(js))
-
-        # the number as written is the largest it can be
-        if self.num_batts == len(bank):
-            return int("".join(bank))
-        
-        i = 0
-        last_i = 0
-        joltage = ""
-        k = len(bank)
-        n = 0
-
-        # sort the batteries largest to smallest, keeping track of the original order 
-        sorted_bank = sorted(bank, reverse=True)
-        
-        inds = []
-        for batt in sorted_bank:
-            inds.extend([x for x in stringutils.indices(batt, bank) if x not in inds])
-        batt_index = [(i, bank[i]) for i in inds]
-       
-        #debug_print(f"BANK {bank} SORTED {sorted_bank} INDS {inds} BI {batt_index}")
-        debug_print(f"BANK {bank}")
-        start = 0
-        digits = []
-        i = 0
-        #while not done:
-        sbi = sorted(batt_index, reverse=True, key=operator.itemgetter(1))  #[:self.num_batts]
-        #digits = [x[1] for x in sorted(sbi, key=operator.itemgetter(0))]
-        debug_print(f"SBI {sbi}")
-        i, d = sbi[start]
-        digits.append(d)
-        while len(digits) < self.num_batts:
-            #debug_print(f"NEXT {start}")
-            #digits.append(sbi[start][1])
-
-            # get the remaining digits whose index is greater than the current digit's index
-            remainder = [x for x in sbi if x[0] > i]
-            debug_print(f"D {digits} R {remainder}")
-            total = len(digits) + len(remainder)
-            # if the number of digits plus the number remaining is num_batts, add to the 
-            # remainder sorted by index - we're done
-            if total == self.num_batts:
-                digits.extend([x[1] for x in sorted(remainder, key=operator.itemgetter(0))])
-                #debug_print(f"DONE {digits}")
-                break
-
-            # if there are too few, start over with the next largest digt
-            if total < self.num_batts:
-                start += 1
-                i, d = sbi[start]
-                digits = [d]
-                debug_print(f"START OVER AT {start}")
-                continue
-
-            
-            # ???
-            i, d = remainder[0]
-            debug_print(f"NEXT DGIT OK {i} {d}")
-            digits.append(d)
-
-        joltage = "".join(digits)
-        debug_print(f"MAX J ? {joltage}")
-        return int(joltage)
+        return int("".join(self._max_joltage_list(batt_index)))
 
 
     def _max_joltage_sum(self):
