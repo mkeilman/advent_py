ec4aff888bfe4a30322c592b0f96dace5a7c84c3
mkeilman <mvkeilman+github@gmail.com>
2025-12-22 08:38:47 -0700

    2025 day 7 part 2 ckp

--- year_2025/Day_07.py
+++ year_2025/Day_07.py
@@ -44,89 +44,64 @@ class AdventDay(Day.Base):
         n, all_pos = self._propagate()
         debug_print(f"NUM SPLITS {n}")
         #self._print_beams(all_pos)
-        m, paths = self._num_paths(all_pos)
+        m, paths = self._num_paths(all_pos[1:])
         debug_print(f"NUM PATHS {m}")
         #for p in paths:
-            #self._print_beams([p])
-            #debug_print(p)
+        #    self._print_beams([p])
+        #    debug_print(p)
         return n
  
 
-    def _np(self, beam_positions, curr_pos=None, depth=0, n=1, paths=None):
+    def _num_paths(self, beam_positions, curr_pos=None, depth=0):
         
         if not beam_positions:
-            #debug_print(f"{depth} END OF GRID")
             return 0, []
         
-
+        n = 0
         b0 = beam_positions[0]
         pos = curr_pos or self.entry_pos
         #debug_print(f"{depth} NP {beam_positions} CURR {pos} NEXT ROW {b0}")
         p0 = [pos]
         paths = [p0]
-        # positions below and to either side of the current one; 
+        # positions below and to either side of the current one (if a splitter is below); 
         # no split positions means the beam continues straight down
         next = [x for x in b0 if abs(x[1] - pos[1]) == 1 and (x[0], pos[1]) in self.splitter_pos] or [x for x in b0 if x[1] == pos[1]]
-        n *= len(next)
-        # copy what we have so far - each possible next position sprouts a new path
+        # copy what we have so far - each possible adjacent position sprouts a new path
+        # straight paths do NOT
         num_new = len(next) - 1
-        #debug_print(f"{depth} APPENDING {num_new} COPIES OF {p0}")
-        paths += num_new * [p0[:]]
+        if num_new:
+            n += 1
+        #paths += num_new * [p0[:]]
+        debug_print(f"{depth} LEN {len(paths)} N {n} NEXT {next}")
         
-        #debug_print(f"{depth} {pos} CURR PATHS {paths}; NEXT {next}")
         i = 0
-        #for i, q in enumerate(next):
         for q in next:
-            #debug_print(f"{depth} PATH[{i}] {paths[i]} + {q}")
-            _, r_paths = self._np(beam_positions[1:], curr_pos=q, depth=depth + 1, n=n, paths=paths)
-            if not r_paths:
-                #debug_print(f"{depth} NO NEW PATHS FROM {q}")
-                paths[i].append(q)
-                i += 1
+            debug_print(f"{depth} GET PATHS FOR {q}")
+            m, r_paths = self._num_paths(beam_positions[1:], curr_pos=q, depth=depth + 1)
+            debug_print(f"{depth} M {m} LEN {len(r_paths)}")
+            if not m:
+                #paths[i].append(q)
+                #i += 1
                 continue
-            #debug_print(f"{depth} PATH[{i}] R {r_paths}")
-            # copy what we have so far
             num_r = len(r_paths) - 1
-            #paths += (len(r_paths) - 1) * [paths[i][:]]
-            #debug_print(f"{depth} I {i}")
+            #num_r = m - 1
+            debug_print(f"{depth} M {m} VS NUM R {num_r}")
             #debug_print(f"{depth} ADDING {num_r} COPIES OF {paths[i]} INTO {paths} AT {i} FOR {r_paths}")
             # insert new paths
-            #paths = paths[:i] + num_r * [paths[i][:]] + paths[i:]
-            for _ in range(num_r):
-                #paths.append(paths[i][:])
-                paths.append(paths[i][:])
-            #debug_print(f"{depth} AFTER INSERT {paths} PATHS")
-            for j, r in enumerate(r_paths):
-            #for r in r_paths:
-                #debug_print(f"{depth} I {i} PATH[{i + j}] {paths[i + j]} ADD ARR {r}")
-                for p in r:
-                    #debug_print(f"{depth} PATH[{i}] ADD POS {p}")
-                    paths[i + j].append(p)
-                    #paths[i].append(p)
-                #debug_print(f"{depth} PATH[{i + j}] NOW {paths[i + j]}")
-            #i += 1
-            i = len(paths) - 1
-        #debug_print(f"{depth} FINAL {paths}")
-        return len(paths), paths
-
-
-    def _num_paths(self, beam_positions, depth=0):
-        return self._np(beam_positions[1:])
-        n = 0
-        #n = 1
-        #debug_print(f"{depth} GET P FROM {beam_positions}")
-        for i in range(len(beam_positions) - 1):
-            m = 1
-            b1 = beam_positions[i]
-            b2 = beam_positions[i + 1]
-            debug_print(f"{depth} GET P FROM {b1} - {b2}")
-            for r in b1:
-                next = [x for x in b2 if abs(x[1] - r[1]) == 1] or [x for x in b2 if x[1] == r[1]]
-                debug_print(f"{depth} NEXT {next}")
-                #n += len(next)
-                m *= len(next)
+            debug_print(f"{depth} N {n} NP BEFORE {len(paths)}")
+            #for _ in range(num_r):
+            #   paths.append(paths[i][:])
             n += m
-        return n
+            #n += num_r
+            debug_print(f"{depth} N {n} NP AFTER {len(paths)}")
+            #debug_print(f"{depth} AFTER INSERT N {n} LEN {len(paths) - 1}")
+            #for j, r in enumerate(r_paths):
+            #    paths[i + j].extend(r)
+                #debug_print(f"{depth} PATH[{i + j}] NOW {paths[i + j]}")
+            #i = len(paths) - 1
+        #n += i
+        debug_print(f"{depth} DONE N {n} NP {len(paths)}")
+        return n, paths
 
 
     def _print_beams(self, beam_positions):
